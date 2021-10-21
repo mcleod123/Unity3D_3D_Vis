7dbf5dbefdbc918c59eded3338e6095dd12b53e1
mcleod123 <mcleod123@rambler.ru>
2021-10-21 07:04:55 +0300

    homework phisics

---
+++ Phisics_HW_19_10_2021/Assets/Scripts/PendulumController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PendulumController : MonoBehaviour
+{
+    [SerializeField] private FixedJoint _jointToDestroy;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            StartCollisions();
+            /*
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log("Event");
+            }
+            */
+
+        }
+
+    }
+
+
+    private void StartCollisions()
+    {
+        GameObject sphere = GameObject.Find("Sphere");
+        _jointToDestroy = sphere.GetComponent<FixedJoint>();
+        Destroy(_jointToDestroy);
+    }
+
+
+}
